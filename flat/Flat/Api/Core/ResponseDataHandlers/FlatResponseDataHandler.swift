e68ad0d80db03ca3de0e4efcc7cbc96401d30df2
yunshi <405029644@qq.com>
2021-12-10 17:17:10 +0800

    Logout when JWT expire

--- flat/Flat/Api/Core/ResponseDataHandlers/FlatResponseDataHandler.swift
+++ flat/Flat/Api/Core/ResponseDataHandlers/FlatResponseDataHandler.swift
@@ -12,11 +12,20 @@ import Foundation
 class FlatResponseHandler: ResponseDataHandler {
     func processResponseData<T>(_ data: Data, decoder: JSONDecoder, forResponseType: T.Type) throws -> T where T : Decodable {
         guard let jsonObj = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
-              let statusCode = jsonObj["status"] as? Int,
-              statusCode == 0 else {
-                  let str = String(data: data, encoding: .utf8)
-                  throw ApiError.serverError(message: str ?? "")
-              }
+              let status = jsonObj["status"] as? Int else {
+            throw ApiError.serverError(message: "unknown data type")
+        }
+        // JWT Fail
+        if let code = jsonObj["code"] as? Int, code == 100006 {
+            DispatchQueue.main.async {
+                AuthStore.shared.logout()
+            }
+            throw ApiError.message(message: "JWT expire")
+        }
+        guard status == 0 else {
+            let str = String(data: data, encoding: .utf8)
+            throw ApiError.serverError(message: str ?? "")
+        }
         decoder.setAnyCodingKey("data")
         return (try decoder.decode(AnyKeyDecodable<T>.self, from: data)).result
     }
