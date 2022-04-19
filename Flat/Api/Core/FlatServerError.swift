98b96690530e181b0714d974f64998f50c7f1b70
xuyunshi <405029644@qq.com>
2022-04-19 10:23:26 +0800

    Add error type

--- Flat/Api/Core/FlatServerError.swift
+++ Flat/Api/Core/FlatServerError.swift
@@ -17,6 +17,10 @@ enum FlatApiError: Int, LocalizedError {
     case UnsupportedPlatform
     case JWTSignFailed
     
+    case SMSVerificationCodeInvalid = 110000
+    // Bind when binding already
+    case SMSAlreadyExist = 110001
+    // Bind a phone has registered
     case PhoneRegistered = 110002
     
     case RoomNotFound = 200000
