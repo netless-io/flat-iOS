26043909ba20a79d1ef26ac02877b47353f75edc
xuyunshi <405029644@qq.com>
2022-04-25 12:02:42 +0800

    Update archive configuration

--- archive.sh
+++ archive.sh
@@ -3,6 +3,6 @@ archivename=$buildname.xcarchive
 archivepath=./Archive/$archivename
 ipapath=./iPa
 scheme=Flat-PROD
-configuration=Release
+configuration=Flat_Release
 xcodebuild archive -workspace Flat.xcworkspace -scheme $scheme -configuration $configuration -archivePath $archivepath -destination generic/platform=iOS
 xcodebuild -exportArchive -archivePath $archivepath -exportOptionsPlist export_release.plist -exportPath $ipapath
