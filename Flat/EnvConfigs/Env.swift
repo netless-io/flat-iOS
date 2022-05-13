b5c61f52afba3d115feff70246596fffc6156f0c
xuyunshi <405029644@qq.com>
2022-05-13 15:06:56 +0800

    Add region field

--- Flat/EnvConfigs/Env.swift
+++ Flat/EnvConfigs/Env.swift
@@ -10,11 +10,20 @@
 import Foundation
 
 struct Env {
+    enum Region: String {
+        case CN
+        case US
+    }
+    
     fileprivate func value<T>(for key: String) -> T {
         guard let value = Bundle.main.infoDictionary?[key] as? T else {
             fatalError("Invalid or missing Info.plist key: \(key)")
         }
         return value
+    }
+    
+    var region: Region {
+        Region(rawValue: value(for: "REGION")) ?? .US
     }
     
     var wechatAppId: String {
