5498885193bf0569432c2fff5e42336d1c4dd1af
xuyunshi <405029644@qq.com>
2022-09-08 13:29:04 +0800

    Improve loadedAll background color

--- Flat/Extensions/UITableView/UITableView+loadedAll.swift
+++ Flat/Extensions/UITableView/UITableView+loadedAll.swift
@@ -12,7 +12,7 @@ extension UITableView {
     func showLoadedAll(_ flag: Bool) {
         if !flag { tableFooterView = nil }
         let view = UIView(frame: .init(x: 0, y: 0, width: 0, height: 44))
-        view.backgroundColor = .color(type: .background)
+        view.backgroundColor = backgroundColor
         let label = UILabel()
         label.font = .systemFont(ofSize: 12)
         label.textColor = .color(type: .text, .weak)
