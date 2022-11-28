d2e5c5d8a814f536ea2c44ad9dffab979cc6d4c7
xuyunshi <405029644@qq.com>
2022-11-28 10:57:48 +0800

    Force MBProgressHUD to display on main thread to avoid main thread modify layout crash

--- Flat/Extensions/UIView/UIViewController+Toast.swift
+++ Flat/Extensions/UIView/UIViewController+Toast.swift
@@ -20,17 +20,20 @@ extension UIViewController {
                timeInterval: TimeInterval = 1.5,
                preventTouching: Bool = false) {
         guard !text.isEmpty else { return }
-        MBProgressHUD.hide(for: view, animated: false)
-        let hud = MBProgressHUD.showAdded(to: view, animated: true)
-        hud.bezelView.style = .solidColor
-        hud.bezelView.color = UIColor.black.withAlphaComponent(0.45)
-        hud.backgroundView.style = .solidColor
-        hud.backgroundView.color = UIColor.black.withAlphaComponent(0.05)
-        hud.mode = .text
-        hud.label.textColor = .white
-        hud.label.numberOfLines = 5
-        hud.label.text = text
-        hud.hide(animated: true, afterDelay: timeInterval)
-        hud.isUserInteractionEnabled = preventTouching
+        DispatchQueue.main.async { [weak view] in
+            guard let view = view else { return }
+            MBProgressHUD.hide(for: view, animated: false)
+            let hud = MBProgressHUD.showAdded(to: view, animated: true)
+            hud.bezelView.style = .solidColor
+            hud.bezelView.color = UIColor.black.withAlphaComponent(0.45)
+            hud.backgroundView.style = .solidColor
+            hud.backgroundView.color = UIColor.black.withAlphaComponent(0.05)
+            hud.mode = .text
+            hud.label.textColor = .white
+            hud.label.numberOfLines = 5
+            hud.label.text = text
+            hud.hide(animated: true, afterDelay: timeInterval)
+            hud.isUserInteractionEnabled = preventTouching
+        }
     }
 }
