272b0a3dbaafdd5ce6677b16b4af0831bac774c4
xuyunshi <405029644@qq.com>
2023-02-13 15:13:39 +0800

    Remove dsym upload script

--- uploadDSYM.sh
+++
@@ -1,5 +0,0 @@
-ARCHIVEPATH=$(find $(pwd)/Archive -name "*.xcarchive")
-DSYMPATH=$ARCHIVEPATH/dSYMs
-UPLOAD_TOOL_PATH=$(find $(pwd)/Pods/FirebaseCrashlytics -name "upload-symbols")
-GOOGLESERVICE_PATH=$(find $(pwd)/Flat -name "GoogleService-Info.plist")
-$UPLOAD_TOOL_PATH -gsp $GOOGLESERVICE_PATH -p ios $DSYMPATH
