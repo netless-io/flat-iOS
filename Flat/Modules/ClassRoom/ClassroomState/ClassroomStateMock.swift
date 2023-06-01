d292cf8196daa26474a61fe1b57d851515fd2025
xuyunshi <405029644@qq.com>
2023-06-01 18:12:19 +0800

    mock

--- Flat/Modules/ClassRoom/ClassroomState/ClassroomStateMock.swift
+++ Flat/Modules/ClassRoom/ClassroomState/ClassroomStateMock.swift
@@ -10,6 +10,23 @@ import Foundation
 import RxRelay
 import RxSwift
 
+class RtmMock: RtmProvider {
+    var error: RxRelay.PublishRelay<RtmError> = .init()
+    var p2pMessage: RxRelay.PublishRelay<(data: Data, sender: String)> = .init()
+    func sendP2PMessageFromArray(_ array: [(data: Data, uuid: String)]) -> RxSwift.Single<Void> {
+        .just(())
+    }
+    func sendP2PMessage(data: Data, toUUID UUID: String) -> RxSwift.Single<Void> {
+        .just(())
+    }
+    func login() -> RxSwift.Single<Void> {
+        .just(())
+    }
+    func logout() -> RxSwift.Single<Void> {
+        .just(())
+    }
+}
+
 class ClassroomStateMock: ClassroomStateHandler {
     init() {}
     
@@ -30,11 +47,40 @@ class ClassroomStateMock: ClassroomStateHandler {
         return .just(())
     }
     
+    var users: [RoomUser] = []
+    var c = 0
+    lazy var m: Observable<[RoomUser]> = {
+        Observable<Int>
+            .interval(.milliseconds(10), scheduler: MainScheduler.instance)
+            .map { _ -> Int in
+                self.c += 1
+                return self.c
+            }
+            .filter { $0 <= 3000 }
+            .map { id -> [RoomUser] in
+                let id = self.c
+                var user = RoomUser(rtmUUID: id.description, rtcUID: UInt(id), name: id.description, avatarURL: nil)
+                if id > 5 {
+                    user.status = .init(isSpeak: false, isRaisingHand: false, camera: false, mic: false, whiteboard: false)
+                } else {
+                    user.status = .init(isSpeak: true, isRaisingHand: false, camera: false, mic: false, whiteboard: true)
+                }
+                self.users.append(user)
+
+                print("--::: id ::: \(id)")
+//                if id == 500 {
+//                    self.users.remove(at: 1)
+//                }
+                return self.users
+            }
+            .do { [weak self] users in
+                let pairs = users.map { ($0.rtmUUID, $0)}
+                self?.currentOnStageUsers = .init(uniqueKeysWithValues: pairs)
+            }
+    }()
+    
     func members() -> RxSwift.Observable<[RoomUser]> {
-        let users = (1...100).map {
-            RoomUser(rtmUUID: $0.description, rtcUID: $0, name: $0.description, avatarURL: nil)
-        }
-        return .just([]).delay(.seconds(1), scheduler: MainScheduler.instance)
+        m
     }
     
     func memberNameQueryProvider() -> UserInfoQueryProvider {
