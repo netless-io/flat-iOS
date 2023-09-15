ea93a5aa4dc577385bfd5b51fc0f373c9970f222
xuyunshi <405029644@qq.com>
2023-09-15 16:20:30 +0800

    Update CI

--- archive.sh
+++ archive.sh
@@ -2,7 +2,8 @@ buildname=$(date +%s)
 archivename=$buildname.xcarchive
 archivepath=./Archive/$archivename
 ipapath=./iPa
-scheme=Flat-PROD
-configuration=Flat_Release
+scheme=$1
+configuration=$2
+exportOptionsPlist=$3
 xcodebuild archive -workspace Flat.xcworkspace -scheme $scheme -configuration $configuration -archivePath $archivepath -destination generic/platform=iOS
-xcodebuild -exportArchive -archivePath $archivepath -exportOptionsPlist export_release.plist -exportPath $ipapath
+xcodebuild -exportArchive -archivePath $archivepath -exportOptionsPlist $exportOptionsPlist -exportPath $ipapath
