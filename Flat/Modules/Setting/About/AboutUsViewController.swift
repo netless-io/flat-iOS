bc46f2b9048785be7650ba5784d4e1449f0669ce
xuyunshi <405029644@qq.com>
2024-03-05 16:54:53 +0800

    China ICP info display

--- Flat/Modules/Setting/About/AboutUsViewController.swift
+++ Flat/Modules/Setting/About/AboutUsViewController.swift
@@ -12,12 +12,30 @@ import UIKit
 import Whiteboard
 
 class AboutUsViewController: UIViewController {
+    @IBOutlet weak var libButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onTap)))
+        if Env().region == .CN {
+            let beianButton = UIButton(type: .system)
+            beianButton.setTitle("沪ICP备14053584号-25A >", for: .normal)
+            beianButton.titleLabel?.font = .systemFont(ofSize: 12)
+            beianButton.addTarget(self, action: #selector(onClickBeian), for: .touchUpInside)
+            view.addSubview(beianButton)
+            beianButton.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.bottom.equalTo(libButton.snp.top).offset(-10)
+            }
+        }
     }
 
+    @objc func onClickBeian(_ sender: Any) {
+        let controller = SFSafariViewController(url: .init(string: "https://beian.miit.gov.cn")!)
+        present(controller, animated: true, completion: nil)
+    }
+    
     @objc func onTap() {
         versionLabel.text = versionLabel.text == "v\(Env().version) (\(Env().build))" ? "Whiteboard v\(WhiteSDK.version())" : "v\(Env().version) (\(Env().build))"
     }
