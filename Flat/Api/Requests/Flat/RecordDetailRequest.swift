cb69c1c4d088e6e024e7280b73b5048ded45207d
xuyunshi <405029644@qq.com>
2024-03-19 16:33:59 +0800

    Support custom base url for replay launch item

--- Flat/Api/Requests/Flat/RecordDetailRequest.swift
+++ Flat/Api/Requests/Flat/RecordDetailRequest.swift
@@ -13,5 +13,6 @@ struct RecordDetailRequest: FlatRequest {
 
     var path: String { "/v1/room/record/info" }
     var task: Task { .requestJSONEncodable(encodable: ["roomUUID": uuid]) }
+    var customBaseURL: String? { Env().customBaseUrlFor(roomUUID: uuid) }
     let responseType = RecordDetailInfo.self
 }
